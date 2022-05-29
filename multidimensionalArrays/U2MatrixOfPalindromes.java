547ada91a5db119c91f5cc828ba29bbb6e24bb86
AnzhelikaKoteva <kotevaanzhelika@gmail.com>
2022-05-29 16:38:34 +0300

    Update U2MatrixOfPalindromes.java

--- multidimensionalArrays/U2MatrixOfPalindromes.java
+++ multidimensionalArrays/U2MatrixOfPalindromes.java
@@ -13,19 +13,26 @@ public class U2MatrixOfPalindromes {
         int cols = line[1];
         String[][] matrix = new String[rows][cols];
 
+        readMatrix(rows, cols, matrix);
+        printMatrix(rows, cols, matrix);
+    }
+
+    private static void printMatrix(int rows, int cols, String[][] matrix) {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void readMatrix(int rows, int cols, String[][] matrix) {
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 char outside = (char) ('a' + r);
                 char middle = (char) (outside + c);
                 matrix[r][c] = String.valueOf(outside) + middle + outside;
             }
-        }
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                    System.out.print(matrix[r][c] + " ");
-            }
-            System.out.println();
         }
     }
 }
